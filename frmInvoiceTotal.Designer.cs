d94295347f94581eb4db003f27683d5b183d2f24
rrduehr <rduehr@smartweb.augustatech.edu>
2026-03-19 05:07:48 -0400

    Add files via upload

---
+++ frmInvoiceTotal.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace InvoiceTotal
+{
+    partial class frmInvoiceTotal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtSubtotal = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btnCalculate = new Button();
+            btnExit = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 30);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 25);
+            label1.TabIndex = 0;
+            label1.Text = "&Subtotal:";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtSubtotal
+            // 
+            txtSubtotal.Location = new Point(187, 24);
+            txtSubtotal.Margin = new Padding(4, 5, 4, 5);
+            txtSubtotal.Name = "txtSubtotal";
+            txtSubtotal.Size = new Size(165, 31);
+            txtSubtotal.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(30, 78);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(148, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Discount Percent:";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(30, 127);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(156, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Discount Amount:";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(30, 175);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(53, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Total:";
+            label4.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(91, 237);
+            btnCalculate.Margin = new Padding(4, 5, 4, 5);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(107, 38);
+            btnCalculate.TabIndex = 2;
+            btnCalculate.Text = "&Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(223, 237);
+            btnExit.Margin = new Padding(4, 5, 4, 5);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(107, 38);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(187, 72);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(167, 31);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(187, 121);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(167, 31);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(187, 169);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(167, 31);
+            textBox3.TabIndex = 12;
+            // 
+            // frmInvoiceTotal
+            // 
+            AcceptButton = btnCalculate;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnExit;
+            ClientSize = new Size(399, 302);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculate);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtSubtotal);
+            Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "frmInvoiceTotal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Invoice Total";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtSubtotal;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button btnCalculate;
+        private Button btnExit;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ frmInvoiceTotal.cs
@@ -0,0 +1,15 @@
+namespace InvoiceTotal
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDiscountPct_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
